3e705eb772c583ba767973d42af93c9202d7589c
KadafiBelajar <bibibibu9900@gmail.com>
2025-11-01 17:40:06 +0700

    Fix sound file paths from /public/ to /static/

--- static/assets-config.js
+++ static/assets-config.js
@@ -11,14 +11,14 @@ const ASSETS_CONFIG = {
     player: {
         // Animasi idle (karakter diam)
         idle: {
-            image: '/public/assets/characters/player/idle.gif',  // atau .webp, .png
+            image: '/static/assets/characters/player/idle.gif',  // atau .webp, .png
             width: 150,   // lebar dalam pixel
             height: 150   // tinggi dalam pixel
         },
         
         // Animasi saat menyerang
         attack: {
-            image: '/public/assets/characters/player/attack.gif',
+            image: '/static/assets/characters/player/attack.gif',
             width: 150,
             height: 150,
             duration: 500  // durasi animasi dalam milidetik
@@ -26,7 +26,7 @@ const ASSETS_CONFIG = {
         
         // Animasi saat kena serangan
         hit: {
-            image: '/public/assets/characters/player/hit.gif',
+            image: '/static/assets/characters/player/hit.gif',
             width: 150,
             height: 150,
             duration: 500
@@ -42,14 +42,14 @@ const ASSETS_CONFIG = {
     boss: {
         // Animasi idle (boss diam)
         idle: {
-            image: '/public/assets/characters/boss/idle.gif',
+            image: '/static/assets/characters/boss/idle.gif',
             width: 150,
             height: 150
         },
         
         // Animasi saat menyerang
         attack: {
-            image: '/public/assets/characters/boss/attack.gif',
+            image: '/static/assets/characters/boss/attack.gif',
             width: 150,
             height: 150,
             duration: 500
@@ -57,7 +57,7 @@ const ASSETS_CONFIG = {
         
         // Animasi saat kena serangan
         hit: {
-            image: '/public/assets/characters/boss/hit.gif',
+            image: '/static/assets/characters/boss/hit.gif',
             width: 150,
             height: 150,
             duration: 500
@@ -73,7 +73,7 @@ const ASSETS_CONFIG = {
     sounds: {
         // Background music (main theme)
         backgroundMusic: {
-            src: '/public/assets/sounds/background-music.mp3',
+            src: '/static/assets/sounds/background-music.mp3',
             volume: 0.3,    // volume 0.0 - 1.0
             loop: true,     // apakah musik berulang
             enabled: true   // enable/disable musik
@@ -81,70 +81,70 @@ const ASSETS_CONFIG = {
         
         // Sound effect saat player menyerang
         playerAttack: {
-            src: '/public/assets/sounds/player-attack.MP3',
+            src: '/static/assets/sounds/player-attack.MP3',
             volume: 0.5,
             enabled: true
         },
         
         // Sound effect saat boss menyerang
         bossAttack: {
-            src: '/public/assets/sounds/boss-attack.MP3',
+            src: '/static/assets/sounds/boss-attack.MP3',
             volume: 0.5,
             enabled: true
         },
         
         // Sound effect saat player kena damage (gunakan boss-attack sebagai fallback)
         playerHit: {
-            src: '/public/assets/sounds/boss-attack.MP3',
+            src: '/static/assets/sounds/boss-attack.MP3',
             volume: 0.4,
             enabled: true
         },
         
         // Sound effect saat boss kena damage (gunakan player-attack sebagai fallback)
         bossHit: {
-            src: '/public/assets/sounds/player-attack.MP3',
+            src: '/static/assets/sounds/player-attack.MP3',
             volume: 0.4,
             enabled: true
         },
         
         // Sound effect saat heal
         heal: {
-            src: '/public/assets/sounds/heal.MP3',
+            src: '/static/assets/sounds/heal.MP3',
             volume: 0.5,
             enabled: true
         },
         
         // Sound effect saat jawaban benar (gunakan victory sebagai fallback)
         correct: {
-            src: '/public/assets/sounds/victory.MP3',
+            src: '/static/assets/sounds/victory.MP3',
             volume: 0.6,
             enabled: true
         },
         
         // Sound effect saat jawaban salah (gunakan defeat sebagai fallback)
         wrong: {
-            src: '/public/assets/sounds/defeat.MP3',
+            src: '/static/assets/sounds/defeat.MP3',
             volume: 0.5,
             enabled: true
         },
         
         // Sound effect saat victory
         victory: {
-            src: '/public/assets/sounds/victory.MP3',
+            src: '/static/assets/sounds/victory.MP3',
             volume: 0.7,
             enabled: true
         },
         
         // Sound effect saat defeat
         defeat: {
-            src: '/public/assets/sounds/defeat.MP3',
+            src: '/static/assets/sounds/defeat.MP3',
             volume: 0.6,
             enabled: true
         },
         
         // Sound effect timer warning (10 detik terakhir)
         timerWarning: {
-            src: '/public/assets/sounds/timer-warning.MP3',
+            src: '/static/assets/sounds/timer-warning.MP3',
             volume: 0.3,
             enabled: true
         }
